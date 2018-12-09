37307341eb5febcc39fb250e9d6bf583f4839c76
André Oliveira <andre@outlook.pt>
2018-12-09 18:56:49 +0000

    Master - Update Icon

--- src/com/stockmanager/controllers/DialogLocationController.java
+++ src/com/stockmanager/controllers/DialogLocationController.java
@@ -31,8 +31,6 @@ public class DialogLocationController {
 
 	private LocationController locationController;
 
-
-
 	public DialogLocationController(LocationController locationController) {
 		this.locationController = locationController;
 	}
--- src/com/stockmanager/utils/Utilities.java
+++ src/com/stockmanager/utils/Utilities.java
@@ -15,6 +15,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.image.Image;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
@@ -73,6 +74,7 @@ public class Utilities {
 		Node node = getNode(sceneName, obj);
 		Scene scene = new Scene((Parent)node);
 		newWindow.setResizable(false);
+		newWindow.getIcons().add(new Image("file://../Images/logo.png"));
 		newWindow.setScene(scene);
 		newWindow.show();
 	}
