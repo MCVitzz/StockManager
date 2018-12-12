1559edf22860505294d305abb934d4981107638b
Vasco Pereira <vascop_98@hotmail.com>
2018-12-12 08:16:32 +0000

    working

--- src/com/stockmanager/model/PickItem.java
+++ src/com/stockmanager/model/PickItem.java
@@ -9,11 +9,6 @@ public class PickItem {
 
 	private StockVolumeItem volume;
 	private double quantity;
-<<<<<<< HEAD
-	
-=======
->>>>>>> master
-
 
 	public PickItem(StockVolumeItem volume, double quantity) {
 		this.volume = volume;
@@ -26,7 +21,6 @@ public class PickItem {
 
 		try {
 			for(SaleItem si : sale.getItems()) {
-<<<<<<< HEAD
 				ResultSet rs = Database.select("SELECT SVI.Company, SVI.Volume, SVI.Item, SVI.Quantity "
 						+ "FROM stockvolumeitem AS SVI "
 						+ "INNER JOIN saleitem AS SI ON SVI.Company = SI.Company AND SVI.Item = SI.Item "
@@ -42,30 +36,16 @@ public class PickItem {
 					SaleVolumeItem svi = new SaleVolumeItem(si.getCompany(), si.getSale(), rs.getLong("Volume"), si.getItem());
 					svi.setQuantity(rs.getLong("Quantity"));
 					svi.setUnit(si.getUnit());
-					
+
 					sv.save();
 					svi.save();
-					
+
 					if(saleqnt < 0) {
 						pck.setQuantity(rs.getDouble("Quantity")+saleqnt);
 						svi.setQuantity(rs.getDouble("Quantity")+saleqnt);
 					}
-					
+
 					pickingList.add(pck);
-=======
-				ResultSet rs = Database.select("SELECT SVI.Volume, SVI.Item, SVI.Quantity"
-						+ "FROM stockvolumeitem AS SVI "
-						+ "INNER JOIN saleitem AS SI ON SVI.Company = SI.Company AND SVI.Item = SI.Item "
-						+ "WHERE SI.Company = '" + sale.getCompany() + "' AND SI.Sale = " + sale.getSale() + " GROUP BY SVI.Company, SVI.Item, SVI.Volume;");
-				saleqnt = Double.parseDouble(Database.simpleSelect("Quantity", "saleitem", "Company = '" + sale.getCompany() + "' AND Sale = " + sale.getSale() + " AND Item = '" + rs.getString("Item") + "'"));
-				while(rs.next()) {
-					saleqnt -= rs.getDouble("Quantity");
-					pickingList.add(new PickItem(new StockVolumeItem(si.getCompany(), rs.getLong("Volume"), si.getItem()), rs.getDouble("Quantity")));
-					new StockVolume(si.getCompany(), rs.getLong("Volume"));
-					if(saleqnt < 0) {
-						
-					}
->>>>>>> master
 				}
 			}
 		}
@@ -87,14 +67,8 @@ public class PickItem {
 	public double getQuantity() {
 		return quantity;
 	}
-<<<<<<< HEAD
-	
+
 	public void setQuantity(double quantity) {
 		this.quantity= quantity;
 	}
-=======
->>>>>>> master
-
-
-
 }
