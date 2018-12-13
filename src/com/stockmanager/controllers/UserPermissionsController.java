16db5030eb45e0c293f8f32b545021d001291317
Vasco Pereira <vascop_98@hotmail.com>
2018-12-13 14:06:04 +0000

    Nao sei o que isto e

--- src/com/stockmanager/controllers/UserPermissionsController.java
+++ src/com/stockmanager/controllers/UserPermissionsController.java
@@ -1,13 +1,18 @@
 package com.stockmanager.controllers;
 
+import javax.sound.midi.Soundbank;
+
 import com.stockmanager.model.UserPermission;
 
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 
 public class UserPermissionsController {
 
@@ -24,15 +29,65 @@ public class UserPermissionsController {
 
 	public void initialize() {
 		clmnType.setCellValueFactory(new PropertyValueFactory<UserPermission, String>("Type"));
-		clmnAccess.setCellValueFactory(new PropertyValueFactory<UserPermission, String>("Access"));
+		//PropertyValueFactory<UserPermission, String>  a = new PropertyValueFactory<UserPermission, String>("Access");
+//		tblUserPermissions.setRowFactory(t -> new TableRow<UserPermission>() {
+//			@Override
+//		    protected void updateItem(UserPermission item, boolean empty) {
+//				super.updateItem(item, empty);
+//
+//	            if (item == null || empty) {
+//	                setText(null);
+//	                setStyle("");
+//	            } else {
+//	                setItem(item);
+//	                
+//	                UserPermission auxPerson = getTableView().getItems().get(getIndex());
+//	                
+//                	setTextFill(Color.BLACK);
+//	                if (auxPerson.getAccess().equals("Yes"))
+//	                    setStyle("-fx-background-color: GREEN");
+//	                else
+//	                    setStyle("-fx-background-color: RED");
+//	            }
+//	        }
+//		});
+		
+		clmnAccess.setCellFactory(column -> {
+			return new TableCell<UserPermission, String>() {
+		        @Override
+		        protected void updateItem(String item, boolean empty) {
+		            super.updateItem(item, empty); //This is mandatory
+
+		            if (item == null || empty) {
+		            	System.out.println(item);
+		                setText(null);
+		                setStyle("");
+		            } else { //If the cell is not empty
+
+		                setText(item); //Put the String data in the cell
+
+		                //We get here all the info of the Person of this row
+		                UserPermission auxPerson = getTableView().getItems().get(getIndex());
+
+		                // Style all persons wich name is "Edgard"
+		                if (auxPerson.getAccess().equals("Yes")) {
+		                    setStyle("-fx-background-color: green");
+		                } else {
+		                    setStyle("-fx-background-color: red");
+		                }
+		            }
+		        }
+		    };
+		});
+
 		tblUserPermissions.getSelectionModel().setCellSelectionEnabled(true);
 		getData();
 	}
-	
+
 	public void getData() {
 		tblUserPermissions.setItems(FXCollections.observableArrayList(UserPermission.getPremissionsFromUser(user)));
 	}
-	
+
 	public UserPermissionsController(String user) {
 		this.user = user;
 	}
@@ -41,10 +96,10 @@ public class UserPermissionsController {
 	void btnRefresh_OnClick() {
 		getData();
 	}
-	
+
 	@FXML
 	void btnUserPermissions_OnClick() {
-		
+
 	}
 
 	@FXML
@@ -56,5 +111,5 @@ public class UserPermissionsController {
 			MainController.getInstance().checkPermissions(MainController.getInstance().getUser().getPermissions());
 		}
 	}
-	
+
 }
